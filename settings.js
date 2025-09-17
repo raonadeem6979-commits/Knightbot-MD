7f878e7d950ce4b0f509da6fa4c26ee525002fdd
raonadeem6979-commits <raonadeem6979@gmail.com>
2025-09-17 12:42:15 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Hashir', // Your name
-  ownerNumber: '923285411279', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '923027474253', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
